e443887bbdfb966d08a501da2024700b928f58e4
Caue Stocco <58923497+xSTCLouDx@users.noreply.github.com>
2026-05-26 21:09:13 -0300

    Update supabase.ts

--- app/supabase.ts
+++ app/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 import 'react-native-url-polyfill/auto';
 
-const supabaseUrl = 'https://yllallpwsyezuqicpzqb.supabase.co';
-const supabaseKey = 'sb_publishable_g7ulHnjlXsOA-NzQcTv0Qg__CH1HJdC';
+const supabaseUrl = 'sua url';
+const supabaseKey = 'sua key';
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
