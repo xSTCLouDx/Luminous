ac245e13bc650618c5bf5178d53bb778fd101961
Caue Stocco <58923497+xSTCLouDx@users.noreply.github.com>
2026-06-23 19:55:07 -0300

    Delete app/supabase.ts

--- app/supabase.ts
+++
@@ -1,7 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-import 'react-native-url-polyfill/auto';
-
-const supabaseUrl = 'sua url';
-const supabaseKey = 'sua key';
-
-export const supabase = createClient(supabaseUrl, supabaseKey);
